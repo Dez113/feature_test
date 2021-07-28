dc5b29bb0d2a2195626b0687918a96f5c3e14691
Igor German <dlyakontakt@gmail.com>
2021-07-28 14:44:04 +0300

    added one more func

--- main.go
+++ main.go
@@ -6,5 +6,10 @@ import "fmt"
 func main(){
 	var someVariable = "disfjldkfjsdf"
 	fmt.Println("Hello_world!", someVariable)
+	fmt.Println(foo())
 	return
+}
+
+func foo(){
+	retrun "Hello from go!"
 }
