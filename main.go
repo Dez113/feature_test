e66d0593d5084d9d62a6a323bc6e1dbadf885f1f
Igor German <dlyakontakt@gmail.com>
2021-07-30 16:45:22 +0300

    #minor testing

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import "fmt"
 func main(){
 	someVariable := "disfjldkfjsdf"
 	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
+	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
+	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
 	fmt.Println(foo())
 	return
 }
