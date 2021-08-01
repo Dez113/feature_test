a08cc9f9f1aff18408111b68658bfd81f8eb34e0
Igor German <dlyakontakt@gmail.com>
2021-08-01 19:58:59 +0300

    te some

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import "fmt"
 func main(){
 	someVariable := "disfjldkfjsdf"
 	fmt.Println("Hello_world2asd31231sdsadasdaswwwdsdasdqqq111a23!", someVariable)
-	fmt.Println("Hello_world2asd31231sdsadasdasdsdasdfswddqqq111a23!", someVariable)
+	fmt.Println("Hello_world2asd3yhjfgh1231sdsadasdasdsdasdfswddqqq111a23!", someVariable)
 	fmt.Println("Hello_world2asd31231sdsadasdasdsdasdqqq111a2389076!", someVariable)
 	fmt.Println(foo())
 	return
