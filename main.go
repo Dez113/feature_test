98ad26f1d74000f27be8e6cedd4d7190d09c9ac2
Igor German <dlyakontakt@gmail.com>
2021-08-01 19:31:22 +0300

    perf(pencil): remove graphiteWidth option
    BREAKING CHANGE: The graphiteWidth option has been removed.
    The default graphite width of 10mm is always used for performance reasons.

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 
 func main(){
 	someVariable := "disfjldkfjsdf"
-	fmt.Println("Hello_world2asd31231sdsadasdasdsdasdqqq111a23!", someVariable)
-	fmt.Println("Hello_world2asd31231sdsadasdasdsdasdqqq111a23!", someVariable)
+	fmt.Println("Hello_world2asd31231sdsadasdaswwwdsdasdqqq111a23!", someVariable)
+	fmt.Println("Hello_world2asd31231sdsadasdasdsdasdfswddqqq111a23!", someVariable)
 	fmt.Println("Hello_world2asd31231sdsadasdasdsdasdqqq111a2389076!", someVariable)
 	fmt.Println(foo())
 	return
