c952a5930bbb2d9119c5df2f99ddb327fdcef7cb
Igor German <dlyakontakt@gmail.com>
2021-08-01 19:13:16 +0300

    feat(clean): delete some lines

--- main.go
+++ main.go
@@ -6,17 +6,6 @@ import "fmt"
 func main(){
 	someVariable := "disfjldkfjsdf"
 	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
-	fmt.Println("Hello_world231231sdsadasdasda23!", someVariable)
 	fmt.Println(foo())
 	return
 }
