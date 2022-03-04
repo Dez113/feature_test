5bcbffb89ab3fe2e77bdbc90c41b77527dbdaa15
Dez113 <dlyakontakt@gmail.com>
2022-03-04 12:25:23 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func getName() string {
+	return "World111!"
+}
+
+func main() {
+	t := time.Now().Unix()
+	s := fmt.Sprintf("%d", t)
+	fmt.Println(s)
+	fmt.Println(s[len(s)-6:])
+}
+
+func foo() string {
+	return "Hello from go!"
+}
